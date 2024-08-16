383f8bc45efe302d30c61e0638dc1b7123cc130e
Nate <natekootstra@gmail.com>
2024-08-15 22:06:22 -0400

    First presentable version.

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+import os.path
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template('home/index.html')
+
+@app.route("/<path>")
+def other_page(path):
+    if os.path.isfile('templates/home/' + path + '.html'):
+        return render_template('home/' + path + '.html')
+    return "File not found"
 
 if __name__ == "__main__":
     context = ("ssl/local.crt", "ssl/local.key")
